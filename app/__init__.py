38e63ed7226f861b91a99e5ca55b375ae4615e68
aydinsak <aydincoolgamer@gmail.com>
2025-12-30 21:32:57 +0700

    ui update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import os
 
 def create_app():
-    app = Flask(__name__)
+    root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    template_dir = os.path.join(root_dir, 'templates')
+    static_dir = os.path.join(root_dir, 'static')
+    
+    app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
     app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
